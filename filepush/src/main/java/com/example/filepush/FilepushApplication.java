de93ae1e87fc16748bd58bd89993c3986a607634
fanlin.zeng <fanlin.zeng@geely.com>
2020-07-24 20:58:39 +0800

    初始化 Signed-off-by: fanlin.zeng <fanlin.zeng@geely.com>

---
+++ filepush/src/main/java/com/example/filepush/FilepushApplication.java
@@ -0,0 +1,13 @@
+package com.example.filepush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilepushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilepushApplication.class, args);
+    }
+
+}
---
+++ filepush/src/test/java/com/example/filepush/FilepushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilepushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
